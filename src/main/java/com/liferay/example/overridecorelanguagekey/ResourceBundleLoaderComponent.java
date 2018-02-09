58429ee153b21184344dc9265509a052c68d6c98
Jesse Rao <jesse.rao@liferay.com>
2018-02-09 12:10:45 -0500

    Add example project that overrides a core language key

---
+++ src/main/java/com/liferay/example/overridecorelanguagekey/ResourceBundleLoaderComponent.java
@@ -0,0 +1,39 @@
+package com.liferay.example.overridecorelanguagekey;
+
+import java.util.Enumeration;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+
+import com.liferay.portal.kernel.language.UTF8Control;
+
+/**
+ * @author jrao
+ */
+@Component(
+	immediate = true,
+	property = {
+		"language.id=en_US"
+	},
+	service = ResourceBundle.class
+)
+public class ResourceBundleLoaderComponent extends ResourceBundle {
+
+	@Override
+	protected Object handleGetObject(String key) {
+		Object object = _resourceBundle.getObject(key);
+		
+		return object;
+	}
+
+	@Override
+	public Enumeration<String> getKeys() {
+		Enumeration<String> keys = _resourceBundle.getKeys();
+		
+		return keys;
+	}
+
+	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
+			"content.Language", UTF8Control.INSTANCE);
+
+}
